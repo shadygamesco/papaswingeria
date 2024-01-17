1e79c28131d1be1f7ab759ef03ae4f9cef2cc787
jonasrhee1208wastaken <121138564+jonasrhee1208wastaken@users.noreply.github.com>
2024-01-17 08:30:00 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//Set save file name(MUST HAVE .data)
+var savefilename = 'example.data';
+
 var swfobject = {};
 
 swfobject.embedSWF = function(url, cont, width, height){
@@ -9,4 +12,79 @@ swfobject.embedSWF = function(url, cont, width, height){
         });
 
     player.load({ url: url });
+}
+
+function getMainSave() {
+  var mainSave = {};
+
+  localStorageSave = Object.entries(localStorage);
+  localStorageSave = btoa(JSON.stringify(localStorageSave));
+  mainSave.localStorage = localStorageSave;
+
+  cookiesSave = document.cookie;
+  cookiesSave = btoa(cookiesSave);
+  mainSave.cookies = cookiesSave;
+
+  mainSave = btoa(JSON.stringify(mainSave));
+  mainSave = CryptoJS.AES.encrypt(mainSave, 'save').toString();
+
+  return mainSave;
+}
+
+function downloadMainSave() {
+  var data = new Blob([getMainSave()]);
+  var dataURL = URL.createObjectURL(data);
+
+  var fakeElement = document.createElement('a');
+  fakeElement.href = dataURL;
+  fakeElement.download = savefilename + '.data';
+  fakeElement.click();
+  URL.revokeObjectURL(dataURL);
+}
+
+function getMainSaveFromUpload(data) {
+  data = CryptoJS.AES.decrypt(data, 'save').toString(CryptoJS.enc.Utf8);
+
+  var mainSave = JSON.parse(atob(data));
+  var mainLocalStorageSave = JSON.parse(atob(mainSave.localStorage));
+  var cookiesSave = atob(mainSave.cookies);
+
+  for (let item in mainLocalStorageSave) {
+    localStorage.setItem(mainLocalStorageSave[item][0], mainLocalStorageSave[item][1]);
+  }
+
+  document.cookie = cookiesSave;
+}
+
+function uploadMainSave() {
+  var hiddenUpload = document.createElement('input');
+  hiddenUpload.type = 'file';
+  hiddenUpload.accept = '.data';
+  document.body.appendChild(hiddenUpload);
+  hiddenUpload.click();
+
+  hiddenUpload.addEventListener('change', function (e) {
+    var files = e.target.files;
+    var file = files[0];
+
+    if (!file) {
+      return;
+    }
+
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      getMainSaveFromUpload(e.target.result);
+
+      var uploadResult = document.querySelector('.upload-result');
+      uploadResult.innerText = 'Uploaded save!';
+      setTimeout(function () {
+        uploadResult.innerText = '';
+      }, 3000);
+    };
+
+    reader.readAsText(file);
+
+    document.body.removeChild(hiddenUpload);
+  });
 }
