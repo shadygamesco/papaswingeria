0081e3b80b82018b03b69115824da8a7aea985a9
jonasrhee1208wastaken <121138564+jonasrhee1208wastaken@users.noreply.github.com>
2024-01-17 08:06:36 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-swfobject.embedSWF = function(url, cont) {
+var swfobject = {};
+
+swfobject.embedSWF = function(url, cont, width, height){
     var ruffle = window.RufflePlayer.newest(),
         player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
-            style: 'position: fixed; top: 0; left: 0; width: 100%; height: 100%;',
+            width: width,
+            height: height,
+            style: 'width: ' + width + 'px; height: ' + height + 'px',
         });
 
     player.load({ url: url });
